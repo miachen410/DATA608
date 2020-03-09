5689cd3901bb149785804cf13b9391584dc305ab
miachen410 <43116221+miachen410@users.noreply.github.com>
2020-03-08 22:18:25 -0400

    Assignment3-2

--- app.R
+++ app.R
@@ -1,44 +1,76 @@
 library(shiny)
 library(tidyverse)
-library(rsconnect)
 
 # Read in data
 cdc <- read_csv("https://raw.githubusercontent.com/charleyferrari/CUNY_DATA_608/master/module3/data/cleaned-cdc-mortality-1999-2010-2.csv")
 
 
-# Define UI for application that shows a barplot
+# Define UI for application that draws the line plot
 ui <- fluidPage(
+  titlePanel("State Mortality Rates vs National Average"),
   
-  titlePanel("Mortality Rates by State in 2010"),
-
   sidebarLayout(
     sidebarPanel(
-      helpText("Select a cause to see States ranked by crude mortality"),
-      uiOutput("causeOutput")
+      helpText("Select cause and state to view the state crude mortality rate 
+               vs national average over time."),
+      uiOutput("causeOutput"),
+      uiOutput("stateOutput")
     ),
-    
     mainPanel(
-      plotOutput("barplot")
+      plotOutput("stateplot")
     )
   )
 )
 
-# Define server logic required to show a barplot
+# Define server logic required to show a line plot
 server <- function(input, output) {
   output$causeOutput <- renderUI({
     selectInput("causeInput", "Cause",
                 unique(cdc$ICD.Chapter),
                 selected = "Neoplasms")
   })
-  output$barplot <- renderPlot({
-    cdc %>% 
-      filter(Year == "2010", ICD.Chapter == input$causeInput) %>% 
-      group_by(State) %>%
-      ggplot(aes(x = reorder(State, Crude.Rate), y = Crude.Rate)) +
-        geom_bar(stat = "identity") +
-        coord_flip() +
-        ylab("Crude Mortality Rate") +
-        xlab("State")
+  
+  output$stateOutput <- renderUI({
+    selectInput("stateInput", "State",
+                unique(cdc$State),
+                selected = "NY")
+  })
+
+  cdc_cause <- reactive({
+    if (is.null(input$causeInput)) {
+      return(NULL)
+    }
+    cdc %>%
+      filter(ICD.Chapter == input$causeInput)
+  })
+  
+  cdc_avg <- reactive({
+    if (is.null(input$causeInput)) {
+      return(NULL)
+    }
+    cdc_cause() %>%
+      group_by(Year) %>%
+      summarize(totalDeath = sum(Deaths), totalPopulation = sum(Population)) %>%
+      mutate(NationalAvg = totalDeath*100000/totalPopulation) 
+      #crude death rate is total death over total population, multiplied by 100,000
+  })
+  
+  state <- reactive({
+    if (is.null(input$stateInput)) {
+      return(NULL)
+    }
+    cdc_cause() %>% filter(State == input$stateInput)
+  })
+  
+  output$stateplot <- renderPlot({
+    if (is.null(cdc_cause())) {
+      return()
+    }
+    ggplot() +
+      geom_line(data = state(), aes(x = Year, y = Crude.Rate, color = "red")) +
+      geom_line(data = cdc_avg(), aes(x = Year, y = NationalAvg, color = "blue")) +
+      labs(title = input$causeInput, x = "", y = "") +
+      scale_color_discrete(labels = c("State", "Nation"))
   })
 }
 
